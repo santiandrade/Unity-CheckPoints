1e810d568442b1f57c6d48b301988760bd5e2c33
santiandrade <santiandrade@hotmail.com>
2015-11-03 21:27:58 +0100

    Initial commit

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+public class CheckPoint : MonoBehaviour 
+{
+    #region Public Variables
+
+    /// <summary>
+    /// Indicate if the checkpoint is activated
+    /// </summary>
+    public bool Activated = false;
+
+    #endregion
+
+    #region Private Variables
+
+    private Animator thisAnimator;
+
+    #endregion
+
+    #region Static Variables
+
+    /// <summary>
+    /// List with all checkpoints objects in the scene
+    /// </summary>
+    public static GameObject[] CheckPointsList;
+
+    #endregion
+
+    #region Static Functions
+    
+    /// <summary>
+    /// Get position of the last activated checkpoint
+    /// </summary>
+    /// <returns></returns>
+    public static Vector3 GetActiveCheckPointPosition()
+    {
+        // If player die without activate any checkpoint, we will return a default position
+        Vector3 result = new Vector3(0, 0, 0);
+
+        if (CheckPointsList != null)
+        {
+            foreach (GameObject cp in CheckPointsList)
+            {
+                // We search the activated checkpoint to get its position
+                if (cp.GetComponent<CheckPoint>().Activated)
+                {
+                    result = cp.transform.localPosition;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    #endregion
+
+    #region Private Functions
+
+    /// <summary>
+    /// Activate the checkpoint
+    /// </summary>
+    private void ActivateCheckPoint()
+    {
+        // We deactive all checkpoints in the scene
+        foreach (GameObject cp in CheckPointsList)
+        {
+            cp.GetComponent<CheckPoint>().Activated = false;
+            cp.GetComponent<Animator>().SetBool("Active", false);
+        }
+
+        // We activated the current checkpoint
+        Activated = true;
+        thisAnimator.SetBool("Active", true);
+    }
+
+    #endregion
+
+    void Start()
+    {
+        thisAnimator = GetComponent<Animator>();
+
+        // We search all the checkpoints in the current scene
+        CheckPointsList = GameObject.FindGameObjectsWithTag("CheckPoint");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // If the player passes through the checkpoint, we activate it
+        if (other.tag == "Player")
+        {
+            ActivateCheckPoint();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Simple player controller based on a Rigidbody
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    #region Public Variables
+    
+    public float Speed = 8.0F;
+    public float JumpForce = 10.0F; 
+
+    #endregion
+
+    #region Private Variables
+    
+    private Rigidbody thisRigidbody;
+    private bool isJumping; 
+
+    #endregion
+
+    void Start()
+    {
+        thisRigidbody = GetComponent<Rigidbody>();
+        isJumping = false;
+    }
+
+    void Update()
+    {
+        if (!isJumping && Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+            thisRigidbody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+        }
+
+        thisRigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, thisRigidbody.velocity.y);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isJumping = false;
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            thisRigidbody.transform.position = CheckPoint.GetActiveCheckPointPosition();
+        }
+    }
+}
